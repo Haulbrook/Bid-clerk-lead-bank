b9a8a6bccad9bde071883ecb52884ace19da69d9
Trey Haulbrook <haulbrookai@gmail.com>
2026-02-11 16:46:57 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LeadBankDashboard from './LeadBankDashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LeadBankDashboard />
+  </React.StrictMode>,
+)
